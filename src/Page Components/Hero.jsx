733efa170ce859c8da3ef9fbc16838e0544641f3
Vivek Mishra <vivekunofficialmail@gmail.com>
2024-11-30 17:31:11 +0530

    updated

--- src/Page Components/Hero.jsx
+++ src/Page Components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import { Container, Row, Col, Button, Card, Image } from 'react-bootstrap';
 import '../Assets/Css/hero.css';
 import { IoIosSettings } from "react-icons/io";
 import { FaRegCirclePlay } from "react-icons/fa6";
@@ -15,7 +15,7 @@ import Hero_profile5 from '../Assets/Images/avatar-5.webp';
 const Hero = () => {
     return (
         <>
-            <Container>
+            <Container className='Hero-background'>
                 <Row className='mt-5'>
                     <Col md={6}>
                         <Button className="btn bg-primary-subtle text-primary rounded-pill d-flex align-items-center justify-content-center" variant="">
@@ -36,63 +36,130 @@ const Hero = () => {
                             <Button variant="white" className='fw-semibold'><FaRegCirclePlay size={24} className='me-2 mx-3' />Get Started</Button>
                         </div>
                     </Col>
-                    <Col md={6} d-flex align-items-center justify-content-center>
-                        <img className='img-thumbnail border-0 position-relative' src={HeroImage} alt="" />
-                        <Card style={{ width: '18rem' }} className=''>
-                            <Card.Body>
-                                <div className="hero-profile d-flex">
-                                    <img className='img-thumbnail border-0 rounded-circle' src={Hero_profile1} alt="" />
-                                    <img className='img-thumbnail border-0 rounded-circle' src={Hero_profile2} alt="" />
-                                    <img className='img-thumbnail border-0 rounded-circle' src={Hero_profile3} alt="" />
-                                    <img className='img-thumbnail border-0 rounded-circle' src={Hero_profile4} alt="" />
-                                    <img className='img-thumbnail border-0 rounded-circle' src={Hero_profile5} alt="" />
-                                    <span class="avatar more rounded-circle d-flex align-items-center bg-primary">12+</span>
-                                </div>
-                                <p class="mb-0 mt-2">12,000+ lorem ipsum dolor sit amet consectetur adipiscing elit</p>
-                            </Card.Body>
-                        </Card>
+                    <Col md={6} className="d-flex align-items-center justify-content-center">
+                        <div className="position-relative">
+                            {/* Hero Image */}
+                            <Image
+                                src={HeroImage}
+                                alt="HeroImage"
+                                className="img-thumbnail border-0"
+                                fluid
+                            />
+
+                            {/* Profile Card */}
+                            <Card
+                                style={{ width: '18rem' }}
+                                className="profile-card position-absolute border-0 shadow-lg"
+                            >
+                                <Card.Body>
+                                    <div className="hero-profile d-flex align-items-center justify-content-center">
+                                        {/* Profile Images */}
+                                        <Image
+                                            src={Hero_profile1}
+                                            alt="Profile 1"
+                                            className="img-thumbnail border-0 rounded-circle"
+                                        />
+                                        <Image
+                                            src={Hero_profile2}
+                                            alt="Profile 2"
+                                            className="img-thumbnail border-0 rounded-circle"
+                                        />
+                                        <Image
+                                            src={Hero_profile3}
+                                            alt="Profile 3"
+                                            className="img-thumbnail border-0 rounded-circle"
+                                        />
+                                        <Image
+                                            src={Hero_profile4}
+                                            alt="Profile 4"
+                                            className="img-thumbnail border-0 rounded-circle"
+                                        />
+                                        <Image
+                                            src={Hero_profile5}
+                                            alt="Profile 5"
+                                            className="img-thumbnail border-0 rounded-circle"
+                                        />
+                                        <span style={{ width: '35px', height: '35px' }} className="rounded-circle bg-primary text-white p-1 d-flex align-items-center justify-content-center">
+                                            12+
+                                        </span>
+
+                                    </div>
+                                    <p className="mb-0 mt-2 text-center">
+                                        12,000+ lorem ipsum dolor sit amet consectetur adipiscing elit
+                                    </p>
+                                </Card.Body>
+                            </Card>
+                        </div>
                     </Col>
+
                 </Row>
-                <Row className='my-3'>
-                    <Col md={3}>
-                        <Card className=' border-0' style={{ width: '18rem' }}>
-                            <Card.Body className='d-flex justify-content-center align-item-centre align-items-center'>
-                                <BsTrophy size={32} className='me-2 text-primary p-1 ' />
-                                <div className="card-Container">
-                                    <Card.Title>3x Won Awards</Card.Title>
-                                    <Card.Text>
-                                        Vestibulum ante ipsum
-                                    </Card.Text>
+                <Row className='mt-5 gy-4'>
+                    <Col md={3} className='mb-2'>
+                        <Card className='border-0'>
+                            <Card.Body>
+                                <div className="stat-items d-flex justify-content-center align-items-center">
+                                    <div className="stat-icon bg-primary-subtle text-primary rounded-circle fs-5 p-3 me-3  d-flex justify-content-center align-items-center">
+                                        <BsTrophy />
+                                    </div>
+                                    <div className="stat-content">
+                                        <h4>3x Won Awards</h4>
+                                        <p className='mb-0'>Vestibulum ante ipsum</p>
+                                    </div>
                                 </div>
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col md={3}>
-                        <Card className=' border-0' style={{ width: '18rem' }}>
-                            <Card.Body className='d-flex justify-content-center align-item-centre align-items-center'>
-                                <BsBriefcase size={32} className='me-2 text-primary p-1 ' />
-                                <div className="card-Container">
-                                    <Card.Title>6.5k Faucibus</Card.Title>
-                                    <Card.Text>
-                                        Nullam quis ante
-                                    </Card.Text>
+                    <Col md={3} className='mb-2'>
+                        <Card className='border-0'>
+                            <Card.Body className=''>
+                                <div className="stat-items d-flex justify-content-center align-items-center">
+                                    <div className="stat-icon bg-primary-subtle text-primary rounded-circle fs-5 p-3 me-3  d-flex justify-content-center align-items-center">
+                                        <BsBriefcase />
+                                    </div>
+                                    <div className="stat-content">
+                                        <h4>6.5k Faucibus</h4>
+                                        <p className='mb-0'>Nullam quis ante</p>
+                                    </div>
                                 </div>
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col md={3}>
-                        <Card className=' border-0' style={{ width: '18rem' }}>
-                            <Card.Body className='d-flex justify-content-center align-item-centre align-items-center'>
-                                <GoGraph size={32} className='me-2 text-primary p-1 ' />
-                                <div className="card-Container">
-                                    <Card.Title>80k Mauris</Card.Title>
-                                    <Card.Text>
-                                        Etiam sit amet orci
-                                    </Card.Text>
+                    <Col md={3} className='mb-2'>
+                        <Card className='border-0'>
+                            <Card.Body>
+                                <div className="stat-items d-flex justify-content-center align-items-center">
+                                    <div className="stat-icon bg-primary-subtle text-primary rounded-circle fs-5 p-3 me-3  d-flex justify-content-center align-items-center">
+                                        <GoGraph />
+                                    </div>
+                                    <div className="stat-content">
+                                        <h4 className=''>80k Mauris</h4>
+                                        <p className='mb-0'>Etiam sit amet orci</p>
+                                    </div>
                                 </div>
                             </Card.Body>
                         </Card>
                     </Col>
+                    <Col md={3} className='mb-2'>
+                        <Card className='border-0'>
+                            <Card.Body>
+                                <div className="stat-items d-flex justify-content-center align-items-center">
+                                    <div className="stat-icon bg-primary-subtle text-primary rounded-circle fs-5 p-3 me-3  d-flex justify-content-center align-items-center">
+                                        <BsAward />
+                                    </div>
+                                    <div className="stat-content">
+                                        <h4>6x Phasellus</h4>
+                                        <p className='mb-0'>Vestibulum ante ipsum</p>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                </Row>
+                {/* <Row className='my-3'>
+                    
+                    
+                    
                     <Col md={3}>
                         <Card className=' border-0' style={{ width: '18rem' }}>
                             <Card.Body className='d-flex justify-content-center align-item-centre align-items-center'>
@@ -106,7 +173,7 @@ const Hero = () => {
                             </Card.Body>
                         </Card>
                     </Col>
-                </Row>
+                </Row> */}
             </Container>
         </>
     )
