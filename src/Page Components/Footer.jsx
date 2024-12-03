8874a89477f4db0b9b02714d46e8ad9e83a8ae84
Vivek Mishra <vivekunofficialmail@gmail.com>
2024-12-04 00:36:05 +0530

    updated

--- src/Page Components/Footer.jsx
+++ src/Page Components/Footer.jsx
@@ -6,12 +6,15 @@ import { RiTwitterXLine, RiFacebookCircleFill, RiInstagramLine, RiLinkedinBoxFil
 const Footer = () => {
     return (
         <>
-            <Container className='py-3'>
+            <Container className='py-5'>
                 <Row>
                     <Col md={4}>
-                        <span className='footer-heading fw-semibold text-dark'>iLanding</span>
 
-                        <div class="footer-contact pt-3">
+                        <div className="footer-header mb-4">
+                            <span className='fw-semibold text-dark fs-3'>iLanding</span>
+                        </div>
+
+                        <div class="footer-contact">
                             <p className='mb-1'>A108 Adam Street</p>
                             <p>New York, NY 535022</p>
                             <p class="mt-3 mb-1">
@@ -23,60 +26,60 @@ const Footer = () => {
                                 <span>info@example.com</span>
                             </p>
                         </div>
-                        <div class="social-links d-flex mt-4 gap-2 fs-5">
-                            <a href="#" className='d-flex align-items-center justify-content-center border rounded-circle text-dark p-2'><RiTwitterXLine /></a>
-                            <a  href="#" className='d-flex align-items-center justify-content-center border rounded-circle text-dark p-2'><RiFacebookCircleFill /></a>
-                            <a  href="#" className='d-flex align-items-center justify-content-center border rounded-circle text-dark p-2'><RiInstagramLine /></a>
-                            <a  href="#" className='d-flex align-items-center justify-content-center border rounded-circle text-dark p-2'><RiLinkedinBoxFill /></a>
+                        <div className="social-links d-flex mt-4 gap-2 fs-5">
+                            <a href="#" className='d-flex align-items-center justify-content-center border border-secondary rounded-circle text-dark footer-icon p-2'><RiTwitterXLine /></a>
+                            <a href="#" className='d-flex align-items-center justify-content-center border border-secondary rounded-circle text-dark footer-icon p-2'><RiInstagramLine /></a>
+                            <a href="#" className='d-flex align-items-center justify-content-center border border-secondary rounded-circle text-dark footer-icon p-2'><RiFacebookCircleFill /></a>
+                            <a href="#" className='d-flex align-items-center justify-content-center border border-secondary rounded-circle text-dark footer-icon p-2'><RiLinkedinBoxFill /></a>
                         </div>
                     </Col>
                     <Col md={2}>
-                        <h4>Useful Links</h4>
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About us</a></li>
-                            <li><a href="#">Services</a></li>
-                            <li><a href="#">Terms of service</a></li>
-                            <li><a href="#">Privacy policy</a></li>
+                        <h4 className='fs-6 mb-2'>Useful Links</h4>
+                        <ul className="list-unstyled">
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Home</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>About us</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Services</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Terms of service</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Privacy policy</a></li>
                         </ul>
                     </Col>
                     <Col md={2}>
-                        <h4>Our Services</h4>
-                        <ul>
-                            <li><a href="#">Web Design</a></li>
-                            <li><a href="#">Web Development</a></li>
-                            <li><a href="#">Product Management</a></li>
-                            <li><a href="#">Marketing</a></li>
-                            <li><a href="#">Graphic Design</a></li>
+                        <h4 className='fs-6 mb-2'>Our Services</h4>
+                        <ul className="list-unstyled">
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Web Design</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Web Development</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Product Management</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Marketing</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Graphic Design</a></li>
                         </ul>
                     </Col>
                     <Col md={2}>
-                        <h4>Hic solutasetp</h4>
-                        <ul>
-                            <li><a href="#">Molestiae accusamus iure</a></li>
-                            <li><a href="#">Excepturi dignissimos</a></li>
-                            <li><a href="#">Suscipit distinctio</a></li>
-                            <li><a href="#">Dilecta</a></li>
-                            <li><a href="#">Sit quas consectetur</a></li>
+                        <h4 className='fs-6 mb-2'>Hic solutasetp</h4>
+                        <ul className="list-unstyled">
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Molestiae accusamus iure</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Excepturi dignissimos</a></li>
+                            <li className='mb-2 footer-list'><a href="#" className='text-decoration-none text-secondary footer-list'>Suscipit distinctio</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Dilecta</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Sit quas consectetur</a></li>
                         </ul>
                     </Col>
                     <Col md={2}>
-                        <h4>Nobis illum</h4>
-                        <ul>
-                            <li><a href="#">Ipsam</a></li>
-                            <li><a href="#">Laudantium dolorum</a></li>
-                            <li><a href="#">Dinera</a></li>
-                            <li><a href="#">Trodelas</a></li>
-                            <li><a href="#">Flexo</a></li>
+                        <h4 className='fs-6 mb-2'>Nobis illum</h4>
+                        <ul className="list-unstyled">
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Ipsam</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Laudantium dolorum</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Dinera</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Trodelas</a></li>
+                            <li className='mb-2'><a href="#" className='text-decoration-none text-secondary footer-list'>Flexo</a></li>
                         </ul>
                     </Col>
                 </Row>
                 <hr />
                 <Row>
                     <div className="copyright text-center">
-                        <p>© <span>Copyright</span> <strong class="px-1 sitename">iLanding</strong> <span>All Rights Reserved</span></p>
+                        <p>© <span>Copyright</span> <strong className="px-1 sitename">iLanding</strong> <span>All Rights Reserved</span></p>
                         <div class="credits">
-                            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                            Designed by <a className='text-decoration-none' href="https://bootstrapmade.com/">BootstrapMade</a>
                         </div>
                     </div>
                 </Row>
@@ -85,4 +88,4 @@ const Footer = () => {
     )
 }
 
-export default Footer
+export default Footer;
--- src/Page Components/MyNavbar.jsx
+++ src/Page Components/MyNavbar.jsx
@@ -1,59 +1,173 @@
 import React from 'react';
 import '../Assets/Css/MyNavbar.css';
-import '../Assets/Css/hero.css';
-import { Container, Row, Col, Nav, Navbar, NavDropdown, Dropdown, Button } from 'react-bootstrap'
+import { Container, Row, Col, Nav, Navbar, NavDropdown, Dropdown, Button } from 'react-bootstrap';
 
 const MyNavbar = () => {
     return (
         <>
-            <Container fluid className='hero-background'>
-            <Container>
-                <Row className=''>
-                    <Col md={12} xs={12} lg={12}>
-                        <div className="header">                            
-                            <Navbar expand="lg" variant='white' className="bg-white text-dark rounded-pill my-3 px-3 py-1 custom-navbar d-flex justify-content-center align-items-center">
-                                <Container className='bg-white d-flex justify-content-between'>
-                                    <Navbar.Brand href="#home" className='fs-4 bg-white '>iLanding</Navbar.Brand>
-                                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                                    <Navbar.Collapse className='bg-white' id="basic-navbar-nav">
-                                        <Nav className="mx-auto">
-                                            <Nav.Link className='text-primary bg-white fs-6 px-2' href="#hero">Home</Nav.Link>
-                                            <Nav.Link className='text-dark bg-white px-2' href="#about">About</Nav.Link>
-                                            <Nav.Link className='text-dark bg-white px-2' href="#features">Features</Nav.Link>
-                                            <Nav.Link className='text-dark bg-white px-2' href="#services">Services</Nav.Link>
-                                            <Nav.Link className='text-dark bg-white px-2' href="#pricing">Pricing</Nav.Link>
-                                            <NavDropdown className='text-dark bg-white px-2' title="Dropdown" id="basic-nav-dropdown">
+            <Container fluid className="hero-background">
+                <Container>
+                    <Row>
+                        <Col md={12} xs={12} lg={12}>
+                            <div className="header">
+                                <Navbar
+                                    expand="lg"
+                                    variant="white"
+                                    className="bg-white text-dark rounded-pill my-3 px-3 py-1 custom-navbar d-flex justify-content-center align-items-center"
+                                >
+                                    <Container className="bg-white d-flex justify-content-between">
+                                        <Navbar.Brand href="#home" className="fs-4 bg-white">
+                                            iLanding
+                                        </Navbar.Brand>
 
-                                                <NavDropdown.Item href="#action/3.1">Dropdown 1</NavDropdown.Item>
+                                        {/* Flex container for the button and toggle */}
+                                        <div className="d-flex align-items-center order-lg-last">
+                                            <Button
+                                                variant="primary"
+                                                className="rounded-pill px-3 me-2"
+                                                style={{ transition: '0.3s' }}
+                                            >
+                                                Get Started
+                                            </Button>
+                                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                        </div>
 
-                                                <Dropdown className='bg-white'>
-                                                    <Dropdown.Toggle variant="light" className='bg-white border-0' >Dropdown</Dropdown.Toggle>
-                                                    <Dropdown.Menu>
-                                                        <Dropdown.Item href="#/action-1">Dropdown 1</Dropdown.Item>
-                                                        <Dropdown.Item href="#/action-2">Dropdown 2</Dropdown.Item>
-                                                        <Dropdown.Item href="#/action-3">Dropdown 3</Dropdown.Item>
-                                                        <Dropdown.Item href="#/action-3">Dropdown 4</Dropdown.Item>
-                                                        <Dropdown.Item href="#/action-3">Dropdown 5</Dropdown.Item>
-                                                    </Dropdown.Menu>
-                                                </Dropdown>
-                                                <NavDropdown.Item href="#action/3.2">Dropdown 2</NavDropdown.Item>
-                                                <NavDropdown.Item href="#action/3.3">Dropdown 3</NavDropdown.Item>
-                                                <NavDropdown.Item href="#action/3.4">Dropdown 4</NavDropdown.Item>
-                                            </NavDropdown>
-                                            <Nav.Link className='text-dark bg-white' href="#pricing">Contact</Nav.Link>
-                                        </Nav>
-                                    </Navbar.Collapse>
-                                        <Button variant="primary" className='rounded-pill px-3' style={{ transition: '0.3s' }}>Get Started</Button>
-                                </Container>
-                            </Navbar>
-                        </div>
-                    </Col>
-                </Row>
+                                        <Navbar.Collapse className="bg-white" id="basic-navbar-nav">
+                                            <Nav className="mx-auto">
+                                                <Nav.Link className="text-primary bg-white fs-6 px-2" href="#hero">
+                                                    Home
+                                                </Nav.Link>
+                                                <Nav.Link className="text-dark bg-white px-2 custom-Navbar-Hover" href="#about">
+                                                    About
+                                                </Nav.Link>
+                                                <Nav.Link className="text-dark bg-white px-2 custom-Navbar-Hover" href="#features">
+                                                    Features
+                                                </Nav.Link>
+                                                <Nav.Link className="text-dark bg-white px-2 custom-Navbar-Hover" href="#services">
+                                                    Services
+                                                </Nav.Link>
+                                                <Nav.Link className="text-dark bg-white px-2 custom-Navbar-Hover" href="#pricing">
+                                                    Pricing
+                                                </Nav.Link>
+                                                <NavDropdown
+                                                    className="text-dark bg-white px-2 custom-Navbar-Hover"
+                                                    title="Dropdown"
+                                                    id="basic-nav-dropdown"
+                                                >
+                                                    <NavDropdown.Item className="custom-Navbar-Hover" href="#action/3.1">
+                                                        Dropdown 1
+                                                    </NavDropdown.Item>
 
-            </Container>
+                                                    <Dropdown className="bg-white custom-Navbar-Hover">
+                                                        <Dropdown.Toggle
+                                                            variant="light"
+                                                            className="bg-white border-0 custom-Navbar-Hover"
+                                                        >
+                                                            Dropdown
+                                                        </Dropdown.Toggle>
+                                                        <Dropdown.Menu>
+                                                            <Dropdown.Item className="custom-Navbar-Hover" href="#/action-1">
+                                                                Dropdown 1
+                                                            </Dropdown.Item>
+                                                            <Dropdown.Item className="custom-Navbar-Hover" href="#/action-2">
+                                                                Dropdown 2
+                                                            </Dropdown.Item>
+                                                            <Dropdown.Item className="custom-Navbar-Hover" href="#/action-3">
+                                                                Dropdown 3
+                                                            </Dropdown.Item>
+                                                            <Dropdown.Item className="custom-Navbar-Hover" href="#/action-3">
+                                                                Dropdown 4
+                                                            </Dropdown.Item>
+                                                            <Dropdown.Item className="custom-Navbar-Hover" href="#/action-3">
+                                                                Dropdown 5
+                                                            </Dropdown.Item>
+                                                        </Dropdown.Menu>
+                                                    </Dropdown>
+                                                    <NavDropdown.Item className="custom-Navbar-Hover" href="#action/3.2">
+                                                        Dropdown 2
+                                                    </NavDropdown.Item>
+                                                    <NavDropdown.Item className="custom-Navbar-Hover" href="#action/3.3">
+                                                        Dropdown 3
+                                                    </NavDropdown.Item>
+                                                    <NavDropdown.Item className="custom-Navbar-Hover" href="#action/3.4">
+                                                        Dropdown 4
+                                                    </NavDropdown.Item>
+                                                </NavDropdown>
+                                                <Nav.Link className="text-dark bg-white custom-Navbar-Hover" href="#pricing">
+                                                    Contact
+                                                </Nav.Link>
+                                            </Nav>
+                                        </Navbar.Collapse>
+                                    </Container>
+                                </Navbar>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
         </>
-    )
-}
+    );
+};
 
-export default MyNavbar
+export default MyNavbar;
+
+
+
+// import React from 'react';
+// import '../Assets/Css/MyNavbar.css';
+// import { Container, Row, Col, Nav, Navbar, NavDropdown, Dropdown, Button } from 'react-bootstrap'
+
+// const MyNavbar = () => {
+//     return (
+//         <>
+//             <Container fluid className='hero-background'>
+//             <Container>
+//                 <Row className=''>
+//                     <Col md={12} xs={12} lg={12}>
+//                         <div className="header">                            
+//                             <Navbar expand="lg" variant='white' className="bg-white text-dark rounded-pill my-3 px-3 py-1 custom-navbar d-flex justify-content-center align-items-center">
+//                                 <Container className='bg-white d-flex justify-content-between'>
+//                                     <Navbar.Brand href="#home" className='fs-4 bg-white'>iLanding</Navbar.Brand>
+//                                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//                                     <Navbar.Collapse className='bg-white' id="basic-navbar-nav">
+//                                         <Nav className="mx-auto">
+//                                             <Nav.Link className='text-primary bg-white fs-6 px-2' href="#hero">Home</Nav.Link>
+//                                             <Nav.Link className='text-dark bg-white px-2 custom-Navbar-Hover' href="#about">About</Nav.Link>
+//                                             <Nav.Link className='text-dark bg-white px-2 custom-Navbar-Hover' href="#features">Features</Nav.Link>
+//                                             <Nav.Link className='text-dark bg-white px-2 custom-Navbar-Hover' href="#services">Services</Nav.Link>
+//                                             <Nav.Link className='text-dark bg-white px-2 custom-Navbar-Hover' href="#pricing">Pricing</Nav.Link>
+//                                             <NavDropdown className='text-dark bg-white px-2 custom-Navbar-Hover' title="Dropdown" id="basic-nav-dropdown">
+
+//                                                 <NavDropdown.Item className='custom-Navbar-Hover' href="#action/3.1">Dropdown 1</NavDropdown.Item>
+
+//                                                 <Dropdown className='bg-white custom-Navbar-Hover'>
+//                                                     <Dropdown.Toggle variant="light" className='bg-white border-0 custom-Navbar-Hover' >Dropdown</Dropdown.Toggle>
+//                                                     <Dropdown.Menu>
+//                                                         <Dropdown.Item className='custom-Navbar-Hover' href="#/action-1">Dropdown 1</Dropdown.Item>
+//                                                         <Dropdown.Item className='custom-Navbar-Hover' href="#/action-2">Dropdown 2</Dropdown.Item>
+//                                                         <Dropdown.Item className='custom-Navbar-Hover' href="#/action-3">Dropdown 3</Dropdown.Item>
+//                                                         <Dropdown.Item className='custom-Navbar-Hover' href="#/action-3">Dropdown 4</Dropdown.Item>
+//                                                         <Dropdown.Item className='custom-Navbar-Hover' href="#/action-3">Dropdown 5</Dropdown.Item>
+//                                                     </Dropdown.Menu>
+//                                                 </Dropdown>
+//                                                 <NavDropdown.Item className='custom-Navbar-Hover' href="#action/3.2">Dropdown 2</NavDropdown.Item>
+//                                                 <NavDropdown.Item className='custom-Navbar-Hover' href="#action/3.3">Dropdown 3</NavDropdown.Item>
+//                                                 <NavDropdown.Item className='custom-Navbar-Hover' href="#action/3.4">Dropdown 4</NavDropdown.Item>
+//                                             </NavDropdown>
+//                                             <Nav.Link className='text-dark bg-white custom-Navbar-Hover' href="#pricing">Contact</Nav.Link>
+//                                         </Nav>
+//                                     </Navbar.Collapse>
+//                                         <Button variant="primary" className='rounded-pill px-3' style={{ transition: '0.3s' }}>Get Started</Button>
+//                                 </Container>
+//                             </Navbar>
+//                         </div>
+//                     </Col>
+//                 </Row>
+
+//             </Container>
+//             </Container>
+//         </>
+//     )
+// }
+
+// export default MyNavbar;
